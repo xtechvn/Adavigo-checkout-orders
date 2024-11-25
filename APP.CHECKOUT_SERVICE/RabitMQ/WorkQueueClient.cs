12e0745cfa7ae3a81f7b7e9e123bfd326ead1d9d
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-11-25 17:03:00 +0700

    clone from SVN

---
+++ APP.CHECKOUT_SERVICE/RabitMQ/WorkQueueClient.cs
@@ -0,0 +1,122 @@
+﻿using APP.CHECKOUT_SERVICE.Common;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using WEB.CMS.Models.Queue;
+
+namespace APP_CHECKOUT.RabitMQ
+{
+    public class WorkQueueClient
+    {
+        private readonly QueueSettingViewModel queue_setting;
+        private readonly ConnectionFactory factory;
+
+        public WorkQueueClient( )
+        {
+            queue_setting = new QueueSettingViewModel()
+            {
+                host = ConfigurationManager.AppSettings["QUEUE_HOST"],
+                port = Convert.ToInt32(ConfigurationManager.AppSettings["QUEUE_PORT"]),
+                v_host = ConfigurationManager.AppSettings["QUEUE_V_HOST"],
+                username = ConfigurationManager.AppSettings["QUEUE_USERNAME"],
+                password = ConfigurationManager.AppSettings["QUEUE_PASSWORD"],
+            };
+            factory = new ConnectionFactory()
+            {
+                HostName = queue_setting.host,
+                UserName = queue_setting.username,
+                Password = queue_setting.password,
+                VirtualHost = queue_setting.v_host,
+                Port = Protocols.DefaultProtocol.DefaultPort
+            };
+        }
+        public bool SyncES(long id, string store_procedure, string index_es, short project_id)
+        {
+            try
+            {
+                var j_param = new Dictionary<string, object>
+                              {
+                              { "store_name", store_procedure },
+                              { "index_es", index_es },
+                              {"project_type", project_id },
+                              {"id" , id }
+
+                              };
+                var _data_push = JsonConvert.SerializeObject(j_param);
+                // Push message vào queue
+                var response_queue = InsertQueueSimpleDurable(_data_push, ConfigurationManager.AppSettings["QUEUE_SYNC_ES"]);
+                Telegram.pushLog("WorkQueueClient - SyncES[" + id + "][" + store_procedure + "] [" + index_es + "][" + project_id + "]: " + response_queue.ToString());
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return false;
+        }
+        public bool InsertQueueSimple(string message, string queueName)
+        {            
+            
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                try
+                {
+                    channel.QueueDeclare(queue: queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: queueName,
+                                         basicProperties: null,
+                                         body: body);
+                    return true;
+
+                }
+                catch (Exception ex)
+                {
+
+                    return false;
+                }
+            }
+        }
+        public bool InsertQueueSimpleDurable( string message, string queueName)
+        {
+            
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                try
+                {
+                    channel.QueueDeclare(queue: queueName,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: queueName,
+                                         basicProperties: null,
+                                         body: body);
+                    return true;
+
+                }
+                catch (Exception ex)
+                {
+                    
+                    return false;
+                }
+            }
+        }
+    }
+}
