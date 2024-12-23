1f3c898843f7d6294e30bc6e2bdb025ed4a849c2
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-12-23 10:03:04 +0700

    Update OrderHotelRentsService.cs

--- APP.CHECKOUT_SERVICE/Engines/Order/OrderHotelRentsService.cs
+++ APP.CHECKOUT_SERVICE/Engines/Order/OrderHotelRentsService.cs
@@ -624,8 +624,10 @@ namespace APP.CHECKOUT_SERVICE.Engines.Order
 
                     };
                     order_summit.obj_order.SystemType = Common.Common.GetSystemTypeByOrderNo(order_summit.obj_order.OrderNo);
+                    Telegram.pushLog("APP.CHECKOUT_SERVICE - OrderHotelRentsService - Voucher: " +((data_list[0].voucher_code != null && data_list[0].voucher_code.Trim() != "")? data_list[0].voucher_code:"NULL"));
+
                     //--apply voucher:
-                    if(data_list[0].voucher_code!=null && data_list[0].voucher_code.Trim() != "")
+                    if (data_list[0].voucher_code!=null && data_list[0].voucher_code.Trim() != "")
                     {
 
                         var input = new B2BTrackingVoucherRequest
@@ -638,7 +640,7 @@ namespace APP.CHECKOUT_SERVICE.Engines.Order
                         };
                         var voucher_apply = await ApplyVoucher(input);
                         Telegram.pushLog("APP.CHECKOUT_SERVICE - OrderHotelRentsService - ApplyVoucher: " + voucher_apply==null? "NULL": JsonConvert.SerializeObject(voucher_apply));
-                        if (voucher_apply != null && voucher_apply.status == 0)
+                        if(voucher_apply != null && voucher_apply.status == 0)
                         {
 
                             double total_discount = 0;
